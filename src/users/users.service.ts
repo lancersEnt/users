5f6e64d036cade7e21b1069c93336058af262869
Ghassen Saaf <saafghassen@gmail.com>
2023-06-13 16:05:42 +0100

    account activation

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'prisma/prisma.service';
 import { Prisma } from '@prisma/client';
 import * as bcrypt from 'bcrypt';
-import { log } from 'console';
 
 @Injectable()
 export class UsersService {
